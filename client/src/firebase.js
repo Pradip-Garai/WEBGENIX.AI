a5c42055666d3eb2445380b7b28557f815c2cac5
Pradip-Garai <pradipgarai173@gmail.com>
2026-04-19 22:44:32 +0530

    updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GithubAuthProvider, GoogleAuthProvider, PhoneAuthProvider} from 'firebase/auth'
+import {getAuth, GithubAuthProvider, GoogleAuthProvider} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
